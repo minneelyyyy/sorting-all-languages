fe8e25b13e307ee9d683840b68aeca76ee8a3721
April Kahny <aprilkahny@gmail.com>
2023-02-11 17:00:55 -0500

    add C

---
+++ C/src/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int cmp(const void* a, const void* b) {
+	return (*(int*)a - *(int*)b);
+}
+
+int is_number(const char* str) {
+	if (str[0] == '-') {
+		str++;
+	}
+
+	for (size_t i = 0; str[i]; i++) {
+		if (!isdigit(str[i])) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+int main(int argc, char** argv) {
+	size_t size = 0;
+	int* numbers = malloc((argc - 1) * sizeof(int));
+
+	if (numbers == NULL) {
+		fprintf(stderr, "error: EOM\n");
+		return 1;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		if (!is_number(argv[i])) {
+			fprintf(stderr, "error: %s is not a valid number\n", argv[i]);
+			return 1;
+		}
+
+		numbers[size++] = atoi(argv[i]);
+	}
+
+	qsort(numbers, size, sizeof(int), cmp);
+
+	for (int i = 0; i < size; i++) {
+		printf("%d\n", numbers[i]);
+	}
+
+	free(numbers);
+
+	return 0;
+}
